b54dfb19fc99e7849d24df5be5b905cfeaeab99c
Abhinav Kumar Rao <shashankr212@gmail.com>
2026-03-08 09:19:17 +0530

    port fix

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { identifyContact } from './lib/actions';
 console.log("Starting server");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT|| 3000;
 
 app.use(express.json());
 
